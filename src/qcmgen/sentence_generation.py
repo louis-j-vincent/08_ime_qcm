3438360e360909e6757521e5ea3b7a38d5a8dc71
Louis <louisjvincent@gmail.com>
2026-01-16 15:29:33 +0100

    cleanup debug print

--- src/qcmgen/sentence_generation.py
+++ src/qcmgen/sentence_generation.py
@@ -18,10 +18,8 @@ def generate_text(nb_phrases: int = 1, complexity: int = 2):
     api_key = os.getenv("OPENAI_API_KEY")
     if not api_key:
         raise ValueError("API_KEY not found in environment variables")
-        
 
     # Load prompt and call llm
-    print(complexity, nb_phrases)
     llm_prompt = open(project_root.parent / "scripts" / "llm_text_generation_prompt.txt").read().strip()
     llm_prompt += '\n' + f' Complexity : {complexity} - num_paragraphs : {nb_phrases}'
     client = OpenAI(api_key=api_key)
