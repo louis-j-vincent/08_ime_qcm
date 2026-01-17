4c9f2c2326faf94be56b1bbfc5c7d6aa952f2709
Louis <louisjvincent@gmail.com>
2026-01-17 10:52:04 +0100

    fix sentence extraction so that we print the sentence instead of its bbox

--- app/app.py
+++ app/app.py
@@ -479,7 +479,7 @@ if pdf_file is not None:
     for i,page in enumerate(doc):
 
         sentences_and_bboxes = extract_sentence_bboxes(page, num_page = i)
-        sentences.extend([s[1] for s in sentences_and_bboxes])
+        sentences.extend([s[0] for s in sentences_and_bboxes])
         
         doc_dict[f'page_{i}'] = {j: s for j, s in enumerate(sentences_and_bboxes)}  # each elt of sentences_and_bboxes is (sentence,bbox)
 
